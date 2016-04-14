5e6de9e9d49492594bab82d0a788f4c563fd5a4e
J-Henry <JH@JH.local>
2016-04-14 09:11:37 -0400

    Ajax for block

--- app/controllers/blockings_controller.rb
+++ app/controllers/blockings_controller.rb
@@ -2,17 +2,24 @@ class BlockingsController < ApplicationController
   before_action :require_user
 
   def new
-    other_user = User.find(params[:user_id])
-    current_user.block(other_user)
-    flash[:success] = "User #{other_user.username} has been blocked and can no longer follow you"
-    redirect_to :back
+    @other_user = User.find(params[:user_id])
+    current_user.block(@other_user)
+    flash[:success] = "User #{@other_user.username} has been blocked and can no longer follow you"
+
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.js { render 'blocking' }
+    end
   end
 
   def destroy
-    other_user = User.find(params[:user_id])
-    current_user.unblock(other_user)
-    flash[:success] = "User #{other_user.username} has been unblocked.  We're all friends!  Yay!"
-    redirect_to :back
-  end
+    @other_user = User.find(params[:user_id])
+    current_user.unblock(@other_user)
+    flash[:success] = "User #{@other_user.username} has been unblocked.  We're all friends!  Yay!"
 
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.js { render 'blocking' }
+    end
+  end
 end
